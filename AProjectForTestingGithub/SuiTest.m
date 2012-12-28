f4a574405b9528f5f01d0216d900a8fb8cfffdb9
suihf <suihf@suihftekiMac-mini.local>
2012-12-28 10:19:56 +0800

    test

---
+++ AProjectForTestingGithub/SuiTest.m
@@ -0,0 +1,13 @@
+//
+//  SuiTest.m
+//  AProjectForTestingGithub
+//
+//  Created by suihf on 12年12月28日.
+//  Copyright (c) 2012年 iOS_Team. All rights reserved.
+//
+
+#import "SuiTest.h"
+
+@implementation SuiTest
+
+@end
