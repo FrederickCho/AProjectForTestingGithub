1d4e29fd5c88715104c33d923f42bd3d391c7778
Frederick_Cho <chou_frederick@yahoo.com.cn>
2012-12-28 10:05:57 +0800

    commit

---
+++ AProjectForTestingGithub/Classes/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  AProjectForTestingGithub
+//
+//  Created by Eric on 28/12/2012.
+//  Copyright (c) 2012 iOS_Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
